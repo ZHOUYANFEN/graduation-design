36b70b5b662e921a1ff3af2939ba2d4d02fc3eae
luhantao <littletaotaotao@foxmail.com>
2016-03-08 19:04:59 +0800

    添加可以在启动时指定监听端口号

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require('path');
+var port = process.env.PORT || 8080;	//端口号，默认8080，可以用命令行传参 PORT=3000
 
 var app = express();
 //托管public文件夹下静态页面
@@ -11,7 +12,7 @@ app.get('/', function (req, res) {
   res.redirect('index.html');
 });
 
-var server = app.listen(80, function () {
+var server = app.listen(port, function () {
   var host = server.address().address;
   var port = server.address().port;
   console.log('Example app listening at http://%s:%s', host, port);
